296bd1603cc1012a4b042aa438a06ff3354b85dd
JEstiben <31193316+JEstiben@users.noreply.github.com>
2018-11-15 00:00:55 -0600

    Ver inspeccion y Hallazgos
    Se implenmento el exportar a pdf las evaluaciones, se agrego al objeto Hallazgo el atributo diapositiva que sierve como referencia, esto tanto para llenar una evaluacion como para verla

--- SistemaIndicadoresAmbientales/Entity/Evaluacion.cs
+++ SistemaIndicadoresAmbientales/Entity/Evaluacion.cs
@@ -7,7 +7,9 @@ namespace SistemaIndicadoresAmbientales.Entity
 {
     public class Evaluacion
     {
-        
+
+        public string NombrePlanta { get; set; }
+
         public int id_Macrotema { get; set; }
 
         
@@ -17,7 +19,8 @@ namespace SistemaIndicadoresAmbientales.Entity
         public string Cumple { get; set; }
 
         
-        public string hallazgo { get; set; }
+        public string Hallazgo { get; set; }
 
+        public string Diapositiva { get; set; }
     }//class
 }//namespace
