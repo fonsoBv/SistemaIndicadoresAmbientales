c47b4b87207601e512a5757730443345a7b4bbda
bryan <bryan@TOSHIBA>
2018-11-22 02:14:45 -0600

    Consumo combustible Equipo Menor

--- SistemaIndicadoresAmbientales/Controllers/ConsumoCombustibleController.cs
+++ SistemaIndicadoresAmbientales/Controllers/ConsumoCombustibleController.cs
@@ -23,7 +23,7 @@ namespace SistemaIndicadoresAmbientales.Controllers
             Models.EquipoModel equipoModel = new Models.EquipoModel();
             ViewData["vehiculos"] = new SelectList(equipoModel.obtenerVehiculos(), "id_activo_placa", "nombre");
             return View();
-        }//end Regisrar un vatihorimetro
+        }//end Regisrar un consumo (vehiculos)
 
         [HttpPost]
         public ActionResult RegistrarConsumo(Entity.ConsumoCombustible consumo, string vehiculos)
@@ -53,14 +53,58 @@ namespace SistemaIndicadoresAmbientales.Controllers
 
             return View();
 
-        }//insertar
+        }//registrar un consumo de combustible para vehiculos
 
         public ActionResult ListarConsumoCombustibleVehiculos()
         {
             ConsumoCombustibleModel consumoModel = new ConsumoCombustibleModel();
             ModelState.Clear();
             return View(consumoModel.obtenerConsumoCombustibleVehiculos());
-        }
+        }//Lista de consumos (solo vehiculos)
+
+        public ActionResult RegistrarConsumoEqMenor()
+        {
+            Models.EquipoModel equipoModel = new Models.EquipoModel();
+            ViewData["eq_menores"] = new SelectList(equipoModel.obtenerEqMenores(), "id_activo_placa", "nombre");
+            return View();
+        }//end Regisrar un consumo (Eq Menor)
+
+        [HttpPost]
+        public ActionResult RegistrarConsumoEqMenor(Entity.ConsumoCombustible consumo, string eq_menores)
+        {
+            try
+            {
+                Models.EquipoModel equipoModel = new Models.EquipoModel();
+                ViewData["eq_menores"] = new SelectList(equipoModel.obtenerVehiculos(), "id_activo_placa", "nombre");
+
+                consumo.fecha_registro = System.DateTime.Now;
+
+                if (ModelState.IsValid)
+                {
+                    Models.ConsumoCombustibleModel sdb = new Models.ConsumoCombustibleModel();
+                    if (sdb.crearConsumoCombustibleEqMenor(consumo, eq_menores))
+                    {
+                        TempData["success"] = "true";
+                        ModelState.Clear();
+                    }
+                    else
+                    {
+                        TempData["error"] = "false";
+                    }
+                }
+            }
+            catch { };
+
+            return View();
+
+        }//registrar un consumo de combustible para equipo menor
+
+        public ActionResult ListarConsumoCombustibleEqMenor()
+        {
+            ConsumoCombustibleModel consumoModel = new ConsumoCombustibleModel();
+            ModelState.Clear();
+            return View(consumoModel.obtenerConsumoCombustibleEqMenor());
+        }//Lista de consumos (solo equipo menor)
 
     }//fin de clase
 }
