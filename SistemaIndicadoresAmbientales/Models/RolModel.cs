c09e7a193ee3d4d433c4e0a5ea64a7038e75fe23
JEstiben <31193316+JEstiben@users.noreply.github.com>
2018-11-22 23:30:38 -0600

    Agregacion de graficos, cambio en el layout y en el regiter user para agregar el rol

---
+++ SistemaIndicadoresAmbientales/Models/RolModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace SistemaIndicadoresAmbientales.Models
+{
+    public class RolModel
+    {
+
+        private SqlConnection connection;
+
+        public RolModel()
+        {
+            String constring = ConfigurationManager.ConnectionStrings["indicadoresAmbientales"].ToString();
+            connection = new SqlConnection(constring);
+        }//constructor
+
+        public List<Entity.Rol> obtenerRol()
+        {
+            List<Entity.Rol> roles = new List<Entity.Rol>();
+
+            SqlCommand cmd = new SqlCommand("sp_obtenerRol", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            connection.Open();
+            sd.Fill(dt);
+            connection.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                roles.Add(
+                    new Entity.Rol
+                    {
+                        Id_Rol = Convert.ToInt32(dr["Id_Rol"]),
+                        Nombre = Convert.ToString(dr["Nombre"]),
+                    });
+            }
+            return roles;
+        }//obtener todas las plantas del sistemas.
+
+    }//class
+}//namespace
