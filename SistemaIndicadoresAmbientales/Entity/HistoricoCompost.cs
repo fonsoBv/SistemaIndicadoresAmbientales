92afee1afe7ee2142984b7e2fd82646bfe4d8220
fonsoBv <31110577+fonsoBv@users.noreply.github.com>
2019-01-28 20:51:05 -0600

    comit

--- SistemaIndicadoresAmbientales/Entity/HistoricoCompost.cs
+++ SistemaIndicadoresAmbientales/Entity/HistoricoCompost.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace SistemaIndicadoresAmbientales.Entity
+﻿namespace SistemaIndicadoresAmbientales.Entity
 {
     public class HistoricoCompost
     {
-
-        public int Id_Compost { get; set; }
         public int Anio { get; set; }
         public decimal Cantidad { get; set; }
         public int Mes { get; set; }
-
     }
 }
